bafc5cf0f244f84e180fb3fe898ec5c40db551e6
Petr Hank <hank@havit.cz>
2026-02-16 16:17:35 +0100

    Added input for easier navigation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,19 @@
         public MainPage()
         {
             InitializeComponent();
+            // Nastavíme prázdnou HTML stránku, aby WebView byl viditelný
+            WebViewControl.Source = new HtmlWebViewSource
+            {
+                Html = "<html><body style='margin:0;padding:0;background-color:white;'></body></html>"
+            };
+        }
+
+        private void OnNavigateClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(UrlEntry.Text))
+            {
+                WebViewControl.Source = UrlEntry.Text;
+            }
         }
     }
 }
